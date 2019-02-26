d6daa799e92c407837968a8a0f5bdb59f9633716
Agnelo Nelo Ribeiro <agnelo.ribeiro@gmail.com>
2019-02-26 05:26:37 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,12 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(hash)
+#def key_for_min_value(hash)
   hash.min_by {|k,v| k}
+#end
+
+def key_for_min_value(ikea)
+  ikea.min_by do |k,v|
+  return "#{k}"
+end
 end
